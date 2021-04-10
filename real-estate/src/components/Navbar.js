d60354d3eb15254ccf86e57b1b8d83812aae202f
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-09 19:49:38 -0500

    Created Hero Image Slider

--- real-estate/src/components/Navbar.js
+++ real-estate/src/components/Navbar.js
@@ -14,7 +14,6 @@ padding: 1rem 2rem;
 z-index: 100;
 position: fixed;
 width: 100%;
-background: red;
 `
 
 const NavLink = css`
---
+++ real-estate/src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/properties',
+        label: 'View Property',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/properties',
+        label: 'View Property',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$8,420,000',
+        path: '/properties',
+        label: 'View Property',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,240,000',
+        path: '/properties',
+        label: 'View Property',
+        image: ImageFour,
+        alt: 'House'
+    },
+]
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'House in Las Vegas, Nevada',
+//         price: '$4,280,000',
+//         path: '/properties',
+//         label: 'View Property',
+//         image: ImageFive,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in Houston, Texas',
+//         price: '$4,280,000',
+//         path: '/properties',
+//         label: 'View Property',
+//         image: ImageSix,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in Alajuela, Costa Rica',
+//         price: '$4,280,000',
+//         path: '/properties',
+//         label: 'View Property',
+//         image: ImageSeven,
+//         alt: 'House'
+//     },
+// ]
